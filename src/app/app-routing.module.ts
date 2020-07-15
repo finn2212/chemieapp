436007a904aae5f9119de55a6a85ca849d7c9733
Finn Stolle <finn.stolle@cp.ag>
2020-07-15 17:06:57 +0200

    unnötige seiten gelöscht

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,6 @@ const routes: Routes = [
   { path: '', redirectTo: 'calculate', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
   {
-    path: 'places',
-    loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path: 'bookings',
-    loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule),
-    canLoad: [AuthGuard]
-  },
-  {
     path: 'user-info',
     loadChildren: () => import('./userInfos/user-info/user-info.module').then(m => m.UserInfoPageModule),
     canLoad: [AuthGuard]
