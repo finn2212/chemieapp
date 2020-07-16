3e4c3fd771d52377b4d81f96ae706509f0d9297e
Finn Stolle <finn.stolle@cp.ag>
2020-07-16 15:27:05 +0200

    firebase works!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,15 @@
 export const environment = {
   production: true,
   googleMapsAPIKey: 'YOUR_GOOGLE_MAPS_API_KEY',
-  firebaseAPIKey: 'AIzaSyCnBqpGMho5FO1lIhTHIKQFOffevyMGVp8'
+  firebaseAPIKey: 'AIzaSyCnBqpGMho5FO1lIhTHIKQFOffevyMGVp8',
+  firebaseConfig: {
+    apiKey: "AIzaSyCnBqpGMho5FO1lIhTHIKQFOffevyMGVp8",
+    authDomain: "recipe-projekt-a3ac8.firebaseapp.com",
+    databaseURL: "https://recipe-projekt-a3ac8.firebaseio.com",
+    projectId: "recipe-projekt-a3ac8",
+    storageBucket: "recipe-projekt-a3ac8.appspot.com",
+    messagingSenderId: "280259881946",
+    appId: "1:280259881946:web:160bb7c6eaeca9052feee3",
+    measurementId: "G-35JYM5DFFN"
+  }
 };
